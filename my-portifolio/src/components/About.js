97a5482bd48e6feb1f348544f5acf805cb993d0d
marqsbruno <marqsbruno@gmail.com>
2023-02-23 21:30:39 -0300

    atualizações básicas

---
+++ my-portifolio/src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../css/about.css';
+import profile from '../imagens/profile-pic.jpg';
+
+export default function About() {
+  return (
+    <div className="about">
+      <div className="text-container">
+        <p className="about-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, qui possimus! Possimus, deserunt vel explicabo sit inventore facere officiis dolorum, minima maiores vitae iusto nobis doloribus a soluta? Voluptatibus, deserunt.</p>
+        <p className="about-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, qui possimus! Possimus, deserunt vel explicabo sit inventore facere officiis dolorum, minima maiores vitae iusto nobis doloribus a soluta? Voluptatibus, deserunt.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, qui possimus!</p>
+      </div>
+      <img className="profile-pic" src={profile} alt="foto de perfil" />
+    </div>
+  )
+} 
---
+++ my-portifolio/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import '../css/header.css';
+
+export default function Header() {
+  return (
+    <div className="header-container">
+      <h3 className="header-itens">Sobre</h3>
+      <h3 className="header-itens">Projetos</h3>
+    </div>
+  )
+}
