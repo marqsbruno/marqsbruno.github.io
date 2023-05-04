c5019633f00211ec00dad2e88564f84799959e89
marqsbruno <marqsbruno@gmail.com>
2023-05-03 23:15:05 -0300

    atualização css

--- my-portifolio/src/components/About.js
+++ my-portifolio/src/components/About.js
@@ -5,12 +5,18 @@ import profile from '../imagens/profile-pic.jpg';
 
 export default function About() {
   return (
-    <div className="about">
+    <div className="about-container">
       <div className="text-container">
-        <p className="about-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, qui possimus! Possimus, deserunt vel explicabo sit inventore facere officiis dolorum, minima maiores vitae iusto nobis doloribus a soluta? Voluptatibus, deserunt.</p>
-        <p className="about-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, qui possimus! Possimus, deserunt vel explicabo sit inventore facere officiis dolorum, minima maiores vitae iusto nobis doloribus a soluta? Voluptatibus, deserunt.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos, qui possimus!</p>
+        <p className="about-text">Olá, tudo bem?</p>
+        <p className="about-text">Sou desenvolvedor web Full Stack desde 2022, quando concluí o curso da Trybe, com experiência em Javascript, React, Redux, TypeScript, Docker, MySQL, MongoDB, Express, Node.js, assim como noções de arquitetura MSC, POO e Metodologias Ágeis.</p>
+        <p className="about-text">
+        Gosto de entender as várias maneiras como a vida se organiza, porque as coisas são como são e porque elas estão onde estão. Em 2013, isso me levou ao curso de Geografia. Atuando principalmente na área de Cartografia e Georeferenciamento, estagiei em orgãos públicos do Estado de São Paulo, com mapeamento de hidrografia e uso do solo.
+        </p>
+        <p className="about-text"> transição para a área de tecnologia e programação veio da vontade inerente de criar, como forma de expressão, curiosidade e desafio. Meu objetivo é produzir coisas interessantes e curiosas, que possam unir e aguçar o melhor das pessoas.</p>
       </div>
-      <img className="profile-pic" src={profile} alt="foto de perfil" />
+      <div className="img-container">
+        <img className="profile-pic" src={profile} alt="foto de perfil" />
+      </div>
     </div>
   )
 } 
