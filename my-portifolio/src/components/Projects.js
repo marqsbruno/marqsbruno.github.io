957fee107e10f2971995d7b8ccc7060b097b4d4b
marqsbruno <marqsbruno@gmail.com>
2023-05-29 16:05:23 -0300

    projetos base

--- my-portifolio/src/components/Projects.js
+++ my-portifolio/src/components/Projects.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function Projects(){
   return (
     <div className="projects">
-      <h3>projetos</h3>
-      <p>Aqui vai um carrossel talvez?</p>
+      <h3>Projetos</h3>
+      <p>Em construção</p>
     </div>
   )
 }
